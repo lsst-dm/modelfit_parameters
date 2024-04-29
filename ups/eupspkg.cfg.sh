b738c7e918c21dae209a3df5ed20c359b46703dd
taranu <dtaranu@astro.princeton.edu>
2024-04-29 14:36:58 -0700

    Add clean command to eupspkg.cfg.sh

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -5,6 +5,11 @@ build()
     ./build.sh
 }
 
+clean()
+{
+    ./clean.sh
+}
+
 config()
 {
     ([ -d "$MODELFIT_PARAMETERS_DIR" ] && ./clean.sh && meson setup --prefix="$MODELFIT_PARAMETERS_DIR/build-release" \
