68daf327f58b51e8ca3f7a86c6079f78ca1be23d
Dan Taranu <dan.s.taranu@gmail.com>
2024-05-03 12:31:26 -0400

    Document Object class

--- include/lsst/modelfit/parameters/object.h
+++ include/lsst/modelfit/parameters/object.h
@@ -29,11 +29,33 @@
 
 namespace lsst::modelfit::parameters {
 
+/**
+ * A generic object from the parameters library.
+ *
+ * Objects have string representations that are guaranteed to be valid in C++
+ * and should also be valid in Python bindings, if implemented correctly.
+ * The interface may be expanded in the future.
+ */
 class Object {
 public:
-    /// Return a full, callable string representation of this
+    /**
+     * Return a full, callable string representation of this.
+     *
+     * @param name_keywords Whether to prefix arguments with "{name}=",
+     *      where name is the arg name in the header (as with keyword
+     *      arguments in Python).
+     * @param namespace_separator The string to use to delimit namespaces,
+     *      i.e. :: in C++ and . in Python.
+     * @return
+     *      A callable string representation of this, which should return an
+     *      an identical object to this.
+     *
+     * @note The representation with name_keywords=false must be callable
+     * in C++. The representation with name_keywords=true should be callable
+     * in Python, if there are any bindings.
+     */
     virtual std::string repr(bool name_keywords = false) const = 0;
-    /// Return a brief string representation of this
+    /// Return a brief, human-readable string representation of this.
     virtual std::string str() const = 0;
 
     friend std::ostream &operator<<(std::ostream &out, const Object &obj) {
